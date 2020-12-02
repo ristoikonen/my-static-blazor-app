01df7ab45ce814f54d8bc68926f6a243dd469ab5
Risto Ikonen <risto.ikonen@aihw.gov.au>
2020-12-02 15:16:39 +1100

    Update to 5.0

--- Client/Program.cs
+++ Client/Program.cs
@@ -16,6 +16,7 @@ namespace Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
+
         }
     }
 }
